8d92fa8bd6901e4b1eadb8556193d79ea3122f93
Wilson Lee <wilsonlee86@gmail.com>
2015-10-20 22:09:30 -0400

    rough sketch

--- app/assets/javascripts/HenryPrecisionMachining/root.js
+++ app/assets/javascripts/HenryPrecisionMachining/root.js
@@ -1,6 +1,25 @@
 app.controller('RootCtrl', ['$document', '$http', '$scope',
 	function ($document, $http, $scope) {
 
-	console.log(123123)
+	var coordinates = { lat: 40.66837400000001, lng: -74.27831400000002 }
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: coordinates,
+		zoom: 17
+	})
+
+	var infowindow = new google.maps.InfoWindow({
+		content: '<b>Henry Precision Machining</b><br>345 Market Street<br>Kenilworth, NJ 07033'
+	})
+
+	var marker = new google.maps.Marker({
+		position: coordinates,
+		map: map,
+		title: 'Henry Precision Machining'
+	})
+
+	
+		infowindow.open(map, marker)
+	
 	
 }])
