6490d1285d2fa2eff134c2ae073111168df7ed38
Wilson Lee <wilsonlee86@gmail.com>
2015-10-24 15:34:43 -0400

    add map link, phone dial

--- app/assets/javascripts/HenryPrecisionMachining/root.js
+++ app/assets/javascripts/HenryPrecisionMachining/root.js
@@ -1,4 +1,8 @@
 app.controller('RootCtrl', ['$document', '$scope',
 	function ($document, $scope) {
 
+	$scope.openMap = function() {
+		window.open('https://www.google.com/maps?ll=40.668451,-74.280348&z=16&t=m&hl=en-US&gl=US&mapclient=embed&cid=10245295106732961418')
+	}
+
 }])
